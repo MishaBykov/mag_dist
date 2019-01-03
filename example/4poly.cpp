97bc8828766f742e4af80d914a5e71692a584f80
Misha Bykov <mike008@bk.ru>
2019-01-03 16:24:16 +0300

    add example

---
+++ example/4poly.cpp
@@ -0,0 +1,251 @@
+// This program reads polytopes from all4polytopes...txt,
+// find the incidence matrics and some properties
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <windows.h>
+
+// Максимальная длина строки входного файла
+#define LINE_SIZE 2048
+// Размерность многогранников
+#define DIM 4
+// Максимальное число вершин
+#define MAX_VERT 10
+// Максимальной число фасет (гиперграней)
+#define MAX_FACET 128
+
+// Greatest common divisor
+int gcd (int a, int b) {
+    return b ? gcd(b, a % b) : a;
+}
+
+// Выясняем: верно ли, что все риджи -- треугольники?
+// Ридж -- пересечение двух фасет
+bool is_simplicial2(int64_t *matrix, int rows, int cols, FILE *outf){
+    for (int i = 0; i < rows - 1; i++)
+        for (int j = i+1; j < rows; j++){
+            int x = matrix[i] & matrix[j];
+            int num = 0;
+            for (;x != 0; x >>= 1)
+                num += x & 1;
+            if (num > 3){
+                //printf (" Not simplicial2: %d %d", i, j);
+                fprintf (outf, "Not simplicial2: %d %d\n", i, j);
+                return false;
+            }    
+        }
+    return true;        
+}
+
+// Транспонируем матрицу
+void transpose(int64_t *source, int rows, int cols, int64_t *transposed){
+    for (int j = 0; j < cols; j++){
+        transposed[j] = 0;
+        for (int i = 0; i < rows; i++)
+            transposed[j] |= ((source[i] >> j) & 1) << i;
+    }        
+}
+
+// Единственный параметр программы -- имя файла all4polytopes...txt
+int main (int argc, char *argv[])
+{
+ 	if (argc < 2){
+		printf("Usage: 4poly [file name]\n");
+		return 0;
+	}
+
+    FILE *inf = fopen(argv[1], "r");
+	if (inf == NULL){
+		printf ("ERROR: Cann't open file %s\n", argv[1]);
+		return 1;
+	}
+	printf ("Input from %s\n", argv[1]);
+
+    // Результаты работы сохраняем в файл с расширением out
+	char outfname[64];
+	sprintf (outfname, "%s.out", argv[1]);
+	FILE *outf = fopen(outfname, "w");
+	if (outf == NULL){
+		printf ("ERROR: Cann't open file %s\n", outfname);
+		return 1;
+	}
+	printf ("Open file %s for writing\n", outfname);
+    
+	char buffer[LINE_SIZE];
+	char *pch;
+    
+    // Читаем входной файл построчно
+    while (fgets (buffer, LINE_SIZE, inf) != NULL){
+        pch = buffer;
+        // Записываем считанную строку в выходной файл
+        //fprintf (outf, "%s", buffer);
+      	// Считываем f-vector и доп. число за ним
+        int fvector[DIM+1];
+        for (int i = 0; i <= DIM; i++)
+            fvector[i] = strtol (pch+1, &pch, 10);
+        /*    
+        if (fvector[1] + fvector[2] + 2 * (fvector[0] + fvector[3]) == 2 * fvector[4]){
+            fprintf (outf, "Ziegler's condition is true!\n");
+            printf ("Ziegler's condition is true!\n");
+            Sleep(1000); // sleep for 1 sec
+        }
+        */
+        // Если нас интересуют только 2-смежностные многогранники
+        if (fvector[0] * (fvector[0] - 1) != 2 * fvector[1])
+            continue;
+        
+        // Записываем считанную строку в выходной файл
+        fprintf (outf, "%s", buffer);
+        fprintf (outf, "This is 2-neighborly polytope!\n");
+
+        // Сохраняем координаты вершин в массив poly
+        int poly[MAX_VERT][2*DIM];
+        pch--;
+        int lcm = 1; // The least common multiple for denominators of coordinates
+        for (int i = 0; i < fvector[0]; i++){
+            pch += 3;
+            //printf (", (");
+            for (int j = 0; j < DIM; j++){
+                // Считываем числитель дроби
+                poly[i][j] = strtol (pch+1, &pch, 10);
+                //printf (" %2d", poly[i][j]);
+                poly[i][j+DIM] = 1;
+                if (pch[0] == '/'){
+                    // Считываем знаменатель дроби
+                    poly[i][j+DIM] = strtol (pch+1, &pch, 10);
+                    // Находим наименьшее общее кратное
+                    lcm = lcm * poly[i][j+DIM] / gcd(lcm, poly[i][j+DIM]);
+                }    
+            }
+            //printf (")");
+        }
+        printf (" lcm = %d", lcm);
+        // Сохраняем считанный многогранник в формате Avis-Fukuda в файл p.ext
+        FILE *curf = fopen("p.ext", "w");
+        fprintf (outf, "\nPolytope:\nbegin\n%d %d", fvector[0], DIM+1);
+        fprintf (curf, "begin\n%d %d", fvector[0], DIM+1);
+        for (int i = 0; i < fvector[0]; i++){
+            fprintf (outf, "\n 1");
+            fprintf (curf, "\n 1");
+            for (int j = 0; j < DIM; j++){
+                poly[i][j] *= lcm / poly[i][j+DIM];
+                fprintf (outf, " %3d", poly[i][j]);
+                fprintf (curf, " %3d", poly[i][j]);
+            }    
+        }        
+        fprintf (outf, "\nend\n");
+        fprintf (curf, "\nend\n");
+        fclose(curf);
+        // Обрабатываем p.ext с помощью программы skeleton64f.exe
+        system("skeleton64f.exe p.ext --avisfukudaformat --silence --nologinfile --extinc --edges --ridges");
+        // Открываем файл p.ext.out -- результат работы skeleton64f.exe
+        FILE *skelf = fopen("p.ext.out", "r");
+        if (skelf == NULL){
+            fprintf (outf, "ERROR: Cann't open file p.ext.out\n");
+            continue;
+        }
+        bool is_pyramid = false;
+        bool is_simplicial = true;
+        // facet_vertex -- матрица инциденций фасет-вершин
+        // Строка матрицы -- 64-битное целое (строка из 64-х нулей и единиц)
+        int64_t facet_vertex[MAX_FACET];
+        // Считываем информацию из p.ext.out
+        while (fgets (buffer, LINE_SIZE, skelf) != NULL){
+            if (memcmp(buffer, "* Extreme rays:", 15) == 0){
+                fgets (buffer, LINE_SIZE, skelf);
+                // Число фасет
+                int facets = strtol (buffer, &pch, 10);
+                if (facets != fvector[DIM-1]){
+                    fprintf (outf, "ERROR: skeleton facets = %d\n", facets);
+                    fvector[DIM-1] = facets;
+                }
+                continue;
+            }
+            if (memcmp(buffer, "* Edges:", 8) == 0){
+                fgets (buffer, LINE_SIZE, skelf);
+                // Число риджей
+                int ridges = strtol (buffer, &pch, 10);
+                if (ridges != fvector[DIM-2]){
+                    fprintf (outf, "ERROR: skeleton ridges = %d\n", ridges);
+                    fvector[DIM-2] = ridges;
+                }
+                continue;
+            }
+            if (memcmp(buffer, "* Rays-to-inequalities incidence:", 33) == 0){
+                int incnum = 0;
+                fprintf (outf, "Incidence:\n");
+                // Матрица инциденций фасет-вершин
+                for (int i = 0; i < fvector[DIM-1]; i++){
+                    fgets (buffer, LINE_SIZE, skelf);
+                    int fnum = strtol (buffer, &pch, 10);
+                    if (fnum != i+1){
+                        fprintf (outf, "ERROR: fnum = %d != %d\n", fnum, i);
+                    }
+                    pch++;
+                    facet_vertex[i] = 0;
+                    int vnum = 0;
+                    int v;
+                    while (v = strtol (pch, &pch, 10)){
+                        facet_vertex[i] |= (int64_t)1 << (v-1);
+                        vnum++;
+                    }
+                    incnum += vnum;
+                    // Является ли многогранник пирамидой?
+                    if (vnum == fvector[0] - 1)
+                        is_pyramid = true;
+                    // Является ли многогранник симплициальным?    
+                    if (vnum != DIM)
+                        is_simplicial = false;
+                    // Сохраняем одну строку матрицы инциденций в файл    
+                    for (v = 0; v < fvector[0]; v++)
+                        fprintf (outf, " %d", ((facet_vertex[i]) >> v) & 1);
+                    fprintf (outf, "\n");
+                }    
+                if (incnum != fvector[DIM])
+                    fprintf (outf, "ERROR: skeleton incnum = %d\n", incnum);
+                continue;
+            }
+            if (memcmp(buffer, "* Ridges:", 9) == 0){
+                fgets (buffer, LINE_SIZE, skelf);
+                // Число ребер
+                int edges = strtol (buffer, &pch, 10);
+                if (edges != fvector[1]){
+                    fprintf (outf, "ERROR: skeleton edges = %d\n", edges);
+                    fvector[1] = edges;
+                }
+                continue;
+            }
+        }
+        fclose(skelf);
+        // Результаты тестирования сохраняем в файл .out
+        if (2*fvector[1] == fvector[0]*(fvector[0] - 1))
+            fprintf (outf, "2-neighborly\n");
+        if (2*fvector[DIM-2] == fvector[DIM-1]*(fvector[DIM-1] - 1))
+            fprintf (outf, "Dual 2-neighborly\n");
+        if (is_simplicial)
+            fprintf (outf, "Simplicial\n");
+        if (is_pyramid)
+            fprintf (outf, "Pyramid\n");
+        // Проверяем, являются ли все риджи треугольниками    
+        bool is_s2 = is_simplicial2(facet_vertex, fvector[DIM-1], fvector[0], outf);
+        fprintf (outf, "2-simplicial\n");
+        bool is_ss2 = false;
+        if (fvector[DIM-1] <= 64){    
+            int64_t vertex_facet[MAX_VERT];
+            // Транспонируем матрицу
+            transpose(facet_vertex, fvector[DIM-1], fvector[0], vertex_facet);
+            // Верно ли, что каждое ребро является пересечением ровно трех фасет?
+            is_ss2 = is_simplicial2(vertex_facet, fvector[0], fvector[DIM-1], outf);
+            fprintf (outf, "Dual 2-simplicial\n");
+        }
+        else{
+            fprintf (outf, "Cann't test dual 2-simpliciality (%d facets > 64)\n", fvector[DIM-1]);
+        }
+        fprintf (outf, "\n");
+    }
+    fclose(inf);
+    fclose(outf);
+	return 0;
+}
---
+++ example/f-vector.cpp
@@ -0,0 +1,380 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <limits.h>  
+#include <stdint.h>  
+
+#define MAX_VERT 9
+#define MAX_FACET 64
+// The dimension of a polytope
+#define DIM 5
+// The maximal number of all faces of a polytope
+#define MAX_FACES 65536
+// The maximal length of a line in the input file
+#define LINE_SIZE 1024 
+
+FILE *logfile;
+
+typedef struct Test_face Test_face;
+
+struct Test_face{
+	Test_face *next;
+	int64_t vertices;
+	int64_t facets;
+};
+
+int64_t face_arrays[2][MAX_FACES]; // list of faces for dim = k and for dim = k+1
+int ref_to_cur_face_facets[MAX_FACES]; // references to cur_face facets
+
+// Compare lexicographically minimal facet of the convex hull of new_face_vertices (new_face_facets) with cur_face_vertices (cur_face_facets)
+// Return 1 if they are equal. Hence, we can add new_face_vertices to the list of (lexicographically minimal) faces
+// new_face_facets contains facets for new_face_vertices; cur_face_facets contains facets for cur_face_vertices
+inline int compare_min_facet (const int vertices, const int64_t new_face_vertices, const int64_t new_face_facets, const int64_t cur_face_vertices, const int64_t cur_face_facets, const int64_t *vertex_facet)
+{
+	int j, v;
+	int64_t x;
+	int64_t f_list[MAX_VERT]; 
+
+	for (j = 0, x = 1; (new_face_vertices & x) == 0; j++, x <<= 1) ;
+
+	f_list[0] = vertex_facet[j];
+	for (v = 0; j < vertices; j++, x <<= 1)	{
+		if (new_face_vertices & x) {
+			f_list[v+1] = (f_list[v] & vertex_facet[j]);
+			if (f_list[v+1] == new_face_facets)
+				continue;
+			if (cur_face_vertices&x == 0) // cur_face_vertices is not a minimal
+				return 0;
+			v++;
+		}
+	}
+	
+	return (f_list[v] == cur_face_facets);
+}
+
+
+
+// Evaluate f-vector of the incidence matrix 'facet_vertices'
+// rows is the number of facets
+// columns is the number of vertices
+// the procedure uses the following global arrays:
+// vert_facets, faces
+int eval_fvector(int facets, int vertices, const int64_t *facet_vertex, const int64_t *vertex_facet, int64_t *fvector, int *dimension)
+{
+	memset (fvector, 0, sizeof(int)*MAX_VERT);
+
+	if (facets <= 2 || vertices <= 2) {
+		fvector[0] = 2;
+		*dimension = 1;
+		return 0;
+	}
+
+	// Some variables
+	int64_t x, y, begin_bit, intersection;
+	int i, j, m, t, j_begin;
+
+	// Temporary arrays for faces
+	int64_t *faces = face_arrays[0];
+	int64_t *new_faces = face_arrays[1], *buf_faces;
+	
+	// Initialize faces
+	for (i = 1, x = 1; i <= vertices; i++, x <<= 1)
+		faces[i] = x;
+	faces[0] = vertices;
+	fvector[0] = vertices;
+
+	// current face
+	int64_t cur_face_vertices, cur_face_facets;
+	int num_cur_face_facets;
+
+	// candidates will contain a list of new faces (candidates)
+	Test_face candidates[MAX_VERT], *cur_candidate, *next_candidate, *prev_candidate;
+	int num_candidates;
+
+
+	// Main cycle
+	for (*dimension = 1; ; (*dimension)++)
+	{
+		new_faces[0] = 0;
+		// For all faces
+		for (i = 1; i <= faces[0]; i++)
+		{
+			cur_face_vertices = faces[i];
+			// Evaluate facets of closure for cur_face_vertices
+			// Initializing of cur_face_facets
+			for (j = 0, x = 1; (cur_face_vertices & x) == 0; x <<= 1, j++) ;
+			begin_bit = x;
+			j_begin = j;
+			cur_face_facets = vertex_facet[j]; // Init
+			// Evaluate closure
+			j++;
+			x <<= 1;
+			for ( ; j < vertices; j++, x <<= 1)
+				if (cur_face_vertices & x)
+					cur_face_facets &= vertex_facet[j];
+
+			// List of facets of cur_face
+			num_cur_face_facets = 0;
+			for (m = 0, y = 1; m < facets; m++, y <<= 1) {
+				if (cur_face_facets & y) {
+					ref_to_cur_face_facets[num_cur_face_facets] = m;
+					num_cur_face_facets++;
+				}
+			}
+			// End of evaluating of cur_face closure
+			
+			if (num_cur_face_facets == 1) // cur_face is a facet
+				goto Exit;
+
+			// candidates will contain a list of new faces
+			num_candidates = 0;
+			cur_candidate = candidates;
+			// Choose vertex right after the smallest vertex in cur_face_vertices
+			x = (begin_bit << 1);
+			j = j_begin+1;
+			// Adding new vertices
+			for (; j < vertices; x <<= 1, j++)
+			{
+				if ((cur_face_vertices & x) == 0) // If this is a new vertex
+				{
+					// Init a new face closure
+					cur_candidate->facets = cur_face_facets & vertex_facet[j];
+					// Init vertices of a new face
+					cur_candidate->vertices = ~((int64_t)0); // ATTENTION!!!
+					for (t = 0; t < num_cur_face_facets; t++)
+					{
+						m = ref_to_cur_face_facets[t];
+						//y = 1;
+						//y <<= m;
+						y = ((int64_t)1 << m); // ATTENTION!!!
+						if ((cur_candidate->facets) & y)
+							cur_candidate->vertices &= facet_vertex[m];
+					}
+					cur_candidate->next = cur_candidate+1;
+					cur_candidate++;
+					num_candidates++;
+				}
+			}
+			if (cur_candidate != candidates)
+			{
+				cur_candidate--;
+				cur_candidate->next = NULL;
+			}
+			else
+				continue;
+				
+				
+
+			//for (cur_candidate = candidates; ; )
+			for (cur_candidate = candidates; cur_candidate != NULL; )
+			{
+				prev_candidate = cur_candidate;
+				next_candidate = prev_candidate->next;
+				// Comparing cur_candidate with the rest candidates
+				for ( ; next_candidate != NULL; )
+				{
+					intersection = (cur_candidate->vertices) & (next_candidate->vertices);
+					if (intersection == (cur_candidate->vertices))
+					{
+						// If next_candidate is greater, then exclude it
+						prev_candidate->next = next_candidate->next;
+						next_candidate = prev_candidate->next;
+					}
+					else if (intersection == (next_candidate->vertices))
+						// If cur_candidate is greater, then skip to the next candidate
+						goto Next;
+					else
+					{
+						prev_candidate = next_candidate;
+						next_candidate = prev_candidate->next;
+					}
+				}
+				//printf ("end internal cycle candidate: %d\n", cur_candidate->vertices);
+				//j = compare_min_facet(cur_candidate->vertices, cur_face_facets, cur_candidate->facets);
+				// Testing: if the lexicographically smallest facet of cur_candidate
+				// coincides with cur_face_vertices, then adding it to new_faces
+			
+				if (compare_min_facet (vertices, cur_candidate->vertices, cur_candidate->facets, cur_face_vertices, cur_face_facets, vertex_facet))
+				{
+					new_faces[0] += 1;
+					new_faces[new_faces[0]] = cur_candidate->vertices;
+				}
+				Next:
+				cur_candidate = cur_candidate->next;
+			}
+		}
+		fvector[*dimension] = new_faces[0];
+		// Exchange faces and new_faces
+		buf_faces = new_faces;
+		new_faces = faces;
+		faces = buf_faces;
+	}	
+
+	Exit:
+/*	cur_polytope->proper_dim = dim+1; 
+	cur_polytope->pyramid = is_pyramid(faces);
+	cur_polytope->simplicial = is_simplicial(cur_polytope->proper_dim);
+	cur_polytope->simplicial |= ((is_3nghb(cur_polytope)) << 1);
+	cur_polytope->simplicial |= ((is_dual2nghb(cur_polytope)) << 7);*/
+	return 0;
+}
+
+
+// Read combinatorial type of polytope from the format
+// http://www-imai.is.s.u-tokyo.ac.jp/~hmiyata/oriented_matroids/
+// For example: http://www-imai.is.s.u-tokyo.ac.jp/~hmiyata/oriented_matroids/polytope_n8d5.txt
+int read_miyata (char *buffer, int &vertices, int &facets, int64_t *facet_vertex, int64_t *vertex_facet, bool &is_pyramid, bool &is_simplicial){
+	int f, v, read;
+	for (v = 0; v < MAX_VERT; v++)
+		vertex_facet[v] = 0;
+    is_pyramid = false;
+    is_simplicial = true;
+    char *endptr = buffer;
+	int64_t x;
+    vertices = 0;
+	for (f = 0, x = 1; (*endptr) == '['; f++, x <<= 1){
+        if (f > MAX_FACET)
+            break;
+		facet_vertex[f] = 0;
+        //printf("%c", *endptr);
+		for (v = 0; (*endptr) != ']'; v++){
+			read = strtol (endptr+1, &endptr, 10);
+			if (read < 0 || read >= MAX_VERT){
+				printf ("ERROR: Wrong format 2 in line %s\n", buffer);
+				return 2;
+			}
+            if (read >= vertices)
+                vertices = read+1;
+            //printf(" %d", read);
+			facet_vertex[f] |= (1 << read);
+			vertex_facet[read] |= x;
+		}
+        if (v == MAX_VERT - 1)
+            is_pyramid = true;
+        if (v != DIM)
+            is_simplicial = false;
+        //printf("%c", *endptr);
+        endptr += 2;
+    }    
+	facets = f;
+	return 0;
+}
+
+// Write incidence matrices into file
+int write_inc (char *filename, int facets, int vertices, int64_t *facet_vertex, int64_t *vertex_facet){
+	FILE *incfile = fopen(filename, "w");
+	if (incfile == NULL){
+		printf ("ERROR: Cann't open file %s\n", filename);
+		return 1;
+	}
+
+	int i, j;
+	int64_t x;
+	fprintf (incfile, "begin\n%d %d\n", facets, vertices);
+	for (i = 0; i < facets; i++){
+		for (j = 0, x = 1; j < vertices; j++, x <<= 1){
+			fprintf (incfile, " %d", (facet_vertex[i]&x)/x);
+		}
+		fprintf (incfile, "\n");
+	}
+	fprintf (incfile, "end\n");
+
+	fprintf (incfile, "\nbegin\n%d %d\n", vertices, facets);
+	for (i = 0; i < vertices; i++){
+		for (j = 0, x = 1; j < facets; j++, x <<= 1){
+			fprintf (incfile, " %d", (vertex_facet[i]&x)/x);
+		}
+		fprintf (incfile, "\n");
+	}
+	fprintf (incfile, "end\n");
+	fclose (incfile);
+	return 0;
+}
+
+// Write f-vector into file and some additional information
+int write_fvec (FILE *outf, int facets, int vertices, int dimension, int64_t *fvector, bool is_pyramid, bool is_simplicial, char *buffer){
+	fprintf (outf, "%s", buffer);
+	fprintf (outf, "  v: %d, f: %d, d: %d, ", vertices, facets, dimension);
+    if (is_pyramid)
+        fprintf (outf, "pyramid, ");
+    if (is_simplicial)
+        fprintf (outf, "simplicial, ");
+    if (fvector[0] * (fvector[0] - 1) == 2 * fvector[1])
+        fprintf (outf, "2-neighborly, ");
+    fprintf (outf, "\n");
+	fprintf (outf, "  f-vector:");
+	for (int i = 0; i < dimension; i++){
+		fprintf (outf, " %d", fvector[i]);
+	}
+	fprintf (outf, "\n");
+
+	return 0;
+}
+
+
+
+///////////////////////////////////
+//  MAIN
+///////////////////////////////////
+
+// The argument of a program -- file with combinatorial types of 5-polytopes from
+// http://www-imai.is.s.u-tokyo.ac.jp/~hmiyata/oriented_matroids/
+// For example: http://www-imai.is.s.u-tokyo.ac.jp/~hmiyata/oriented_matroids/polytope_n8d5.txt
+int main(int argc, char *argv[]) {
+
+	if (argc < 2){
+		printf("Usage: fvector [inc-file]\n");
+		return 1;
+	}
+
+	FILE *infile = fopen(argv[1], "r");
+	if (infile == NULL){
+		printf ("ERROR: Cann't open file %s\n", argv[1]);
+		return 1;
+	}
+	char filename[255];
+	sprintf(filename, "%s.fvec", argv[1]);
+	FILE *outfile = fopen(filename, "w");
+	if (outfile == NULL){
+		printf ("ERROR: Cann't open file %s\n", filename);
+		return 1;
+	}
+    
+	int facets, vertices = MAX_VERT;
+	int64_t facet_vertex[MAX_FACET], vertex_facet[MAX_VERT];
+    bool is_pyramid, is_simplicial;
+	char buffer[LINE_SIZE];
+    
+	int64_t fvector[MAX_VERT];
+    int dimension;
+    int min_facets = MAX_FACET;
+    int max_facets = 0;
+    while(fgets (buffer, LINE_SIZE, infile) != NULL){
+        // Read combinatorial type of a polytope
+        if (read_miyata (buffer, vertices, facets, facet_vertex, vertex_facet, is_pyramid, is_simplicial) != 0)
+            break;
+        //printf ("v = %d, f = %d\n", vertices, facets);
+        if (facets > MAX_FACET){
+            fprintf (outfile, "ERROR: %d facets > %d\n", facets, MAX_FACET);
+            printf ("ERROR: %d facets > %d\n", facets, MAX_FACET);
+            continue;
+        }        
+        // Evaluate f-vector
+        eval_fvector(facets, vertices, facet_vertex, vertex_facet, fvector, &dimension);
+        if (fvector[0] * (fvector[0] - 1) == 2 * fvector[1]){
+            // Write only 2-neighborly polytopes
+            if (min_facets > fvector[dimension - 1])
+                min_facets = fvector[dimension - 1];
+            if (max_facets < fvector[dimension - 1])
+                max_facets = fvector[dimension - 1];
+            write_fvec (outfile, facets, vertices, dimension, fvector, is_pyramid, is_simplicial, buffer);
+        }    
+    }
+    
+	fprintf (outfile, "MIN facets: %d\nMAX facets: %d\n", min_facets, max_facets);
+    fclose (infile);
+    fclose (outfile);
+	return 0;
+}
