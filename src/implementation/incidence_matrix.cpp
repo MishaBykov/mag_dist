046a5aa15beeb0389ebc03f0a581c734bbcdebd6
misha <mike008@bk.ru>
2019-02-18 00:04:10 +0300

    update tasks

--- src/implementation/incidence_matrix.cpp
+++ src/implementation/incidence_matrix.cpp
@@ -7,6 +7,7 @@
 #include <memory>
 #include <iostream>
 #include <fstream>
+
 #include <incidence_matrix.hpp>
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,6 @@ int main() {
     IncidenceMatrix incidence_matrix;
     incidence_matrix.appendRow("0011001");
     auto i = IncidenceMatrix::readFromFile("../input_files/3d.txt");
-    i[0].printToFile("out.txt");
+    i[0].printToFile("../out.txt");
     return 0;
 }
