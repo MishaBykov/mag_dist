51c925daf0d97aea965609d1bb04554aa4a5a25d
misha <mike008@bk.ru>
2019-04-16 13:33:54 +0300

    clear main

--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,7 @@
 int main() {
     setlocale(LC_ALL, "rus");
 
-    auto v_polyhedron = Polyhedron::readFromFile("../input_files/4d2sc.txt");
 
-    int count_2sc = 0;
-    for (auto &i : *v_polyhedron) {
-        if( Checker::is4d2sc(i) )
-            count_2sc++;
-    }
-
-    std::cout << v_polyhedron->size() << std::endl << count_2sc << std::endl;
 
     return 0;
 }
