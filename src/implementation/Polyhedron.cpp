4a2c96fdd32440d5bad52a978c8c644f56e2c5c2
misha <mike008@bk.ru>
2019-04-23 15:46:00 +0300

    update polyhedron

--- src/implementation/Polyhedron.cpp
+++ src/implementation/Polyhedron.cpp
@@ -12,26 +12,26 @@
 
 Polyhedron::Polyhedron() = default;
 Polyhedron::Polyhedron(unsigned int dimension) : dimension(dimension) {}
-Polyhedron::Polyhedron(unsigned int dimension, std::shared_ptr<IncidenceMatrix> incidenceMatrix){
+Polyhedron::Polyhedron(unsigned int dimension, const std::shared_ptr<IncidenceMatrix>& incidenceMatrix){
     if( checkIncidenceMatrix(incidenceMatrix, dimension) ) {
         this->matrix = incidenceMatrix;
         this->dimension = dimension;
     }
 }
 
-std::shared_ptr<std::vector<PolyhedronSPtr>> Polyhedron::readFromFile(std::string file_name) {
-    std::shared_ptr<std::vector<PolyhedronSPtr>> result = std::make_shared<std::vector<PolyhedronSPtr>>();
+std::vector<PolyhedronSPtr> Polyhedron::readFromFile(const std::string& file_name) {
+    std::vector<PolyhedronSPtr> result;
     std::ifstream file_in(file_name);
 
     if(!file_in.is_open()) {
         std::cout << "Файл[" << file_name << "] не может быть открыт" << std::endl;
-        return nullptr;
+        return result;
     }
 
     while (!file_in.eof()) {
         auto polyhedron = readFromStream(file_in);
         if( polyhedron )
-            result->push_back( polyhedron );
+            result.push_back( polyhedron );
     }
 
     file_in.close();
@@ -77,8 +77,8 @@ std::shared_ptr<Polyhedron> Polyhedron::getFacetIncidenceMatrix(unsigned int ind
     std::shared_ptr<IncidenceMatrix> new_matrix = std::make_shared<IncidenceMatrix>();
     unsigned int new_dimension = dimension - 1;
     unsigned long select_row = matrix->getRow(index_row);
-    for(unsigned int i = 0; select_row != 0; select_row >>= 1, i++){
-        if( select_row & 1 ) {
+    for(unsigned int i = 0; select_row != 0; select_row >>= 1u, i++){
+        if( select_row & 1u ) {
             new_matrix->appendColumn(matrix->getColumn(i));
         }
     }
@@ -113,7 +113,7 @@ void Polyhedron::setMatrix(std::shared_ptr<IncidenceMatrix> new_matrix) {
         std::cout << "new matrix no check" << std::endl;
 }
 
-bool Polyhedron::checkIncidenceMatrix(std::shared_ptr<IncidenceMatrix> incidenceMatrix, unsigned int dimension) {
+bool Polyhedron::checkIncidenceMatrix(const std::shared_ptr<IncidenceMatrix>& incidenceMatrix, unsigned int dimension) {
     if( !incidenceMatrix )
         return false;
     auto sum_columns = incidenceMatrix->sumColumns();
@@ -156,6 +156,14 @@ unsigned int Polyhedron::getDimension() {
 
 bool Polyhedron::isIinitialized() {
     return bool(matrix);
+}
+
+unsigned int Polyhedron::getCountFacets() {
+    return matrix->getCountRow();
+}
+
+unsigned int Polyhedron::getCountVertex() {
+    return matrix->getCountColumn();
 }
 
 
