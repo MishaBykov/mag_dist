c5671ab759298406bd3db18c71404905af0f2f74
misha <mike008@bk.ru>
2019-06-05 15:29:25 +0300

    add Timer

---
+++ src/implementation/Timer.cpp
@@ -0,0 +1,29 @@
+//
+// Created by misha on 05.06.19.
+//
+
+#include <Timer.h>
+#include <iostream>
+#include <ctime>
+#include <ratio>
+#include <chrono>
+
+int main ()
+{
+    using namespace std::chrono;
+
+    steady_clock::time_point t1 = steady_clock::now();
+
+    std::cout << "printing out 1000 stars...\n";
+    for (int i=0; i<1000; ++i) std::cout << "*";
+    std::cout << std::endl;
+
+    steady_clock::time_point t2 = steady_clock::now();
+
+    duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+
+    std::cout << "It took me " << time_span.count() << " seconds.";
+    std::cout << std::endl;
+
+    return 0;
+}
