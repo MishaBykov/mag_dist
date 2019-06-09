babe44c1435b976b0a20e291ee7ce83d85ecfa34
misha <mike008@bk.ru>
2019-06-09 03:31:38 +0300

    add tools

---
+++ tools/generate_table/generate_table.cpp
@@ -0,0 +1,63 @@
+//
+// Created by misha on 08.06.2019.
+//
+
+#include <clocale>
+#include <Polyhedron.h>
+#include <iostream>
+#include <vector>
+
+int main() {
+    setlocale(LC_ALL, "rus");
+
+    std::string name_file = "4d2sc.txt";
+
+    auto v = Polyhedron::readFromFile("re_src/" + name_file);
+
+    std::vector<std::vector<unsigned int>> result;
+
+    for (unsigned int i = 0; i < v.size(); i++) {
+        if (v[i]->getCountVertex() >= result.size()) {
+            result.resize(v[i]->getCountVertex() + 1);
+        }
+        if (v[i]->getCountFacets() >= result[v[i]->getCountVertex()].size()) {
+            result[v[i]->getCountVertex()].resize(v[i]->getCountFacets() + 1);
+        }
+        if (v[i]->getCountVertex() == 0 && v[i]->getCountFacets() == 0)
+            std::cout << i << " ";
+        result[v[i]->getCountVertex()][v[i]->getCountFacets()]++;
+    }
+
+    std::vector<std::string> table(3);
+    std::vector<std::string> table_latex(3);
+
+    for (unsigned int i = 0; i < result.size(); ++i) {
+        for (unsigned int j = 0; j < result[i].size(); ++j) {
+            if (result[i][j]) {
+                table[0] += std::to_string(i) + "\t";
+                table[1] += std::to_string(j) + "\t";
+                table[2] += std::to_string(result[i][j]) + "\t";
+
+                table_latex[0] += std::to_string(i) + " & ";
+                table_latex[1] += std::to_string(j) + " & ";
+                table_latex[2] += std::to_string(result[i][j]) + " & ";
+            }
+        }
+    }
+
+
+    std::ofstream file_result("tables/" + name_file);
+
+    file_result << table[0] << std::endl;
+    file_result << table[1] << std::endl;
+    file_result << table[2] << std::endl << std::endl;
+
+    file_result << "latex:" << std::endl;
+    file_result << table_latex[0] << std::endl;
+    file_result << table_latex[1] << std::endl;
+    file_result << table_latex[2] << std::endl;
+
+    file_result.close();
+
+    return 0;
+}
---
+++ tools/test/test.cpp
@@ -0,0 +1,40 @@
+//
+// Created by misha on 08.06.2019.
+//
+
+#include <clocale>
+#include <Polyhedron.h>
+#include <iostream>
+#include <Checker.h>
+
+int main() {
+    setlocale(LC_ALL, "rus");
+
+    auto v_test = Polyhedron::readFromFile("re_src/4d2sc.txt");
+
+    for (int i = 0; i < v_test.size(); ++i) {
+        auto sc = v_test[i]->getMatrix()->sumColumns();
+        if (sc.empty()){
+            v_test.erase(v_test.begin() + i);
+            i--;
+            continue;
+        }
+        if (sc.back() == 0) {
+            v_test[i]->printToStream(std::cout);
+            v_test[i]->getMatrix()->removeColumn(sc.size() - 1);
+        }
+        for (int j = 0; j < v_test[i]->getCountFacets(); ++j) {
+            if ( !Checker::is3dSimplicial(v_test[i]->getPolyhedronFacet(j) ) )
+            {
+                std::cout << i << std::endl;
+                v_test[i]->printToStream(std::cout);
+                v_test.erase(v_test.begin() + i);
+                i--;
+                break;
+            }
+        }
+    }
+    Polyhedron::printToFile(v_test, "re_src/4d2sc.txt");
+
+    return 0;
+}
