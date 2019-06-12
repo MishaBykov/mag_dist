8a8067f4a80308a17a4369b173f021a6a09766f7
misha <mike008@bk.ru>
2019-06-12 20:21:27 +0300

    update table

--- src/generate_table.h
+++ src/generate_table.h
@@ -6,56 +6,83 @@
 #include <Polyhedron.h>
 #include <iostream>
 #include <vector>
+#include <Checker.h>
 
 int generate_table() {
     setlocale(LC_ALL, "rus");
 
-    std::string name_file = "5d2sc.txt";
+    std::string name_file = "5d2n.txt";
 
     auto v = Polyhedron::readFromFile("re_src/" + name_file);
 
     std::vector<std::vector<unsigned int>> result;
 
     for (unsigned int i = 0; i < v.size(); i++) {
-        if (v[i]->getCountVertex() >= result.size()) {
-            result.resize(v[i]->getCountVertex() + 1);
+        if (v[i]->getCountFacets() >= result.size()) {
+            result.resize(v[i]->getCountFacets() + 1);
         }
-        if (v[i]->getCountFacets() >= result[v[i]->getCountVertex()].size()) {
-            result[v[i]->getCountVertex()].resize(v[i]->getCountFacets() + 1);
+        if (v[i]->getCountVertex() >= result[v[i]->getCountFacets()].size()) {
+            result[v[i]->getCountFacets()].resize(v[i]->getCountVertex() + 1);
         }
-        if (v[i]->getCountVertex() == 0 && v[i]->getCountFacets() == 0)
+        if (v[i]->getCountFacets() == 0 && v[i]->getCountVertex() == 0)
             std::cout << i << " ";
-        result[v[i]->getCountVertex()][v[i]->getCountFacets()]++;
+        result[v[i]->getCountFacets()][v[i]->getCountVertex()]++;
     }
 
-    std::vector<std::string> table(1, "0");
-    std::vector<std::string> table_latex(1);
+    std::vector<std::string> table1;
+    std::vector<std::string> table_latex1;
+    std::vector<std::string> table2(3);
+    std::vector<std::string> table_latex2(3);
 
-    int max_size = 0;
-    for (unsigned int i = 1; i < result.size(); ++i) {
-        if (max_size < result[i].size())
-            max_size = result[i].size();
-        table.emplace_back();
-        table[i] += std::to_string(i);
+    for (unsigned int i = 0; i < result.size(); ++i) {
+        table1.emplace_back();
+        table1[i] += std::to_string(i);
         for (unsigned int j = 0; j < result[i].size(); ++j) {
-            table[i] += "\t" + std::to_string(result[i][j]);
+            table1[i] += "\t" + std::to_string(result[i][j]);
         }
     }
-    for (unsigned int i = 1; i < max_size; ++i) {
-        table[0] += "\t" + std::to_string(i);
+    for (unsigned int i = 0; i < result.size(); ++i) {
+        table_latex1.emplace_back();
+        table_latex1[i] += std::to_string(i);
+        for (unsigned int j = 0; j < result[i].size(); ++j) {
+            table_latex1[i] += " & " + std::to_string(result[i][j]);
+        }
+    }
+
+    for (unsigned int i = 0; i < result.size(); ++i) {
+        for (unsigned int j = 0; j < result[i].size(); ++j) {
+            if (result[i][j]) {
+                table2[0] += std::to_string(i) + "\t";
+                table2[1] += std::to_string(j) + "\t";
+                table2[2] += std::to_string(result[i][j]) + "\t";
+
+                table_latex2[0] += " & " + std::to_string(i);
+                table_latex2[1] += " & " + std::to_string(j);
+                table_latex2[2] += " & " + std::to_string(result[i][j]);
+            }
+        }
     }
 
 
     std::ofstream file_result("tables/" + name_file);
-
-    for (int i = 0; i < table.size(); ++i) {
-        file_result << table[i] << std::endl;
+    file_result << "tables1:" << std::endl;
+    for (int i = 0; i < table1.size(); ++i) {
+        file_result << table1[i] << std::endl;
     }
-
-//    file_result << "latex:" << std::endl;
-//    file_result << table_latex[0] << std::endl;
-//    file_result << table_latex[1] << std::endl;
-//    file_result << table_latex[2] << std::endl;
+    file_result << std::endl << "latex:" << std::endl;
+    for (int i = 0; i < table_latex1.size(); ++i) {
+        file_result << table_latex1[i] << std::endl;
+    }
+    file_result << std::endl;
+    file_result << "tables2:" << std::endl;
+    file_result << table2[0] << std::endl;
+    file_result << table2[1] << std::endl;
+    file_result << table2[2] << std::endl;
+    file_result << std::endl;
+    file_result << "latex2:" << std::endl;
+    file_result << table_latex2[0] << std::endl;
+    file_result << table_latex2[1] << std::endl;
+    file_result << table_latex2[2] << std::endl;
 
     file_result.close();
 
